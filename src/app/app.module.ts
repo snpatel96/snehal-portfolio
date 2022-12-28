0bc03104d0668b5fdcf3907d9c1f87aef003c5ea
snpatel96 <snpatel.dev@gmail.com>
2022-12-28 00:49:55 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IonicModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
